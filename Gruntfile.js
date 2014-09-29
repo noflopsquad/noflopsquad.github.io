f50f00d8e16218b4ef7e8a723acb4ae250baeac0
Sergio Revilla <replymusic@gmail.com>
2014-09-29 18:47:14 +0200

    added CNAME file

--- Gruntfile.js
+++ Gruntfile.js
@@ -307,7 +307,8 @@ module.exports = function (grunt) {
                         '.htaccess',
                         'images/{,*/}*.webp',
                         '{,*/}*.html',
-                        'styles/fonts/{,*/}*.*'
+                        'styles/fonts/{,*/}*.*',
+                        'CNAME'
                     ]
                 }]
             },
