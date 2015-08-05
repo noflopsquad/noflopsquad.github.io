dc068c331c5df1dc2e0fc98cf3fca77bd7bcbe43
elreplicante <replymusic@gmail.com>
2015-08-05 23:59:57 +0200

    changing susy for foundation

--- Gruntfile.js
+++ Gruntfile.js
@@ -115,7 +115,6 @@ module.exports = function (grunt) {
         // Compiles Sass to CSS and generates necessary files if requested
         compass: {
             options: {
-                require: 'susy',
                 sassDir: '<%= yeoman.app %>/styles',
                 cssDir: '.tmp/styles',
                 generatedImagesDir: '.tmp/images/generated',
